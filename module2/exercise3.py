b8ff60af3a81092b04f357ca68bdae7943ad7b7e
Juha Tauriainen <juha@bin.fi>
2024-08-22 10:44:30 +0300

    Possible solution to module 2, exercise 3

---
+++ module2/exercise3.py
@@ -0,0 +1,14 @@
+length = float(input("Give length: "))
+width = input("Give width: ")
+width = float(width)
+
+perimeter = (length * 2) + (width * 2)
+area = width * length
+
+print(f"Perimeter: {perimeter}")
+print(f"Perimeter: {(length * 2) + (width * 2)}")
+print(f"Area: {area:.2f}")
+print(f"Area: {area:<26.2f}")
+print(f"Area: {area}")
+
+print("Perimeter: " + str(perimeter))
